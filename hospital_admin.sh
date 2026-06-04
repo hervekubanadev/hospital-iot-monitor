6af52f1757facec8843b9a30193c739d93e3d026
mndayiragi-hub <m.ndayiragi@alustudent.com>
2026-06-04 13:37:08 +0200

    Final perfect

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+# Member 1: The Architect
+initialize_system(){
+
+     i="active_logs"
+     a="archived_logs"
+     r="reports"
+
+  if [ -d "$i" ];
+     then
+       echo "file $i does exist"
+  else
+    echo "file doesn't exist; creating active_logs directory"
+    mkdir "$i"
+ fi
+
+  if [ -d "$a" ];
+     then
+       echo "file $a does exist"
+  else
+   echo "file doesn't exist; creating archived_logs directory"
+    mkdir "$a"
+ fi
+
+  if [ -d "$r" ];
+     then
+       echo "file $r does exist"
+  else
+    echo "file doesn't exist; creating reports directory"
+    mkdir "$r"
+ fi
+}
 secure_data() {
 
     echo "============================================"
