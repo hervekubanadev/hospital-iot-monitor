c3639369ab1c29259cbeaeaed48f26e04d6812c3
Daniella189-hub <m.nishimwe@alustudent.com>
2026-06-05 22:31:44 +0200

    Enhance hospital_analysis.sh with the right parameters "timestamp" "device-id" "value"
    Added parameters for timestamp, device ID, and value.

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -6,6 +6,9 @@ process_vitals() {
     echo "------------------------------------"
 
     mkdir -p reports
+    timestamp=$1
+    device-id=$2
+    value=$3
 
     grep "CRITICAL" active_logs/heart_rate.log | \
     awk -F',' '{print $1","$2","$3}' \
